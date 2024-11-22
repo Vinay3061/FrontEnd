b0c3ebaff9ed4092fb3ed129456b6b091074ecfb
unknown <vinaykumarhn63@gmail.com>
2024-11-22 10:54:14 +0530

    Initial commit1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Enable App Router
+  },
 };
-
-export default nextConfig;
